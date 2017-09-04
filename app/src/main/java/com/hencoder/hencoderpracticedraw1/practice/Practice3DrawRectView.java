bfc7b94b8ff5aee372e0aeb974c5aa20de277449
walkerys <ys826096638@gmail.com>
2017-09-04 18:24:23 +0800

    完成部分练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +27,12 @@ public class Practice3DrawRectView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRect() 方法画矩形
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(100, 100, 200, 200, paint);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(300, 100, 400, 200, paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +26,8 @@ public class Practice8DrawArcView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        Paint paint = new Paint();
+        canvas.drawArc(100,100,400,600,160,100,true,paint);
+        canvas.drawArc(100,100,400,600,20,40,false,paint);
     }
 }
